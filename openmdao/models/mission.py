2049a7d353d590ded024aeb6274b19022f69b610
Scott Delbecq <scott.delbecq91@gmail.com>
2020-11-30 17:10:38 +0100

    Changed mission approximation to triangular speed profile

--- openmdao/models/mission.py
+++ openmdao/models/mission.py
@@ -11,20 +11,22 @@ class Mission(om.ExplicitComponent):
         # To perform an evaluation instead of a design
         self.options.declare("evaluation", types=bool, default=False)
 
-        # To use potential energy instead of the trajectory analysis
-        self.options.declare("use_potential_energy", types=bool, default=False)
+        # To use potential an approximation of the trajectory analysis
+        self.options.declare("use_approximation", types=bool, default=False)
 
     def setup(self):
 
         self.add_input("data:propeller:number", val=np.nan, units=None)
         self.add_input("data:esc:efficiency", val=np.nan, units=None)
         self.add_input("data:motor:power:hover", val=np.nan, units="W")
+        self.add_input("data:motor:power:takeoff", val=np.nan, units="W")
         self.add_input("data:mission:hover:time", val=np.nan, units="s")
+        self.add_input("data:mission:travel:time", val=2.8, units="s")
         self.add_input("data:mission:travel:displacement:number", val=np.nan, units=None)
 
 
-        self.add_input("data:mission:travel:displacement:height", val=10.0, units="m")
-        self.add_input("data:system:MTOW:guess", val=np.nan, units="kg")
+        # self.add_input("data:mission:travel:displacement:height", val=10.0, units="m")
+        # self.add_input("data:system:MTOW:guess", val=np.nan, units="kg")
 
         self.add_input("data:mission:travel:energy", val=np.nan, units="J")
 
@@ -47,16 +49,19 @@ class Mission(om.ExplicitComponent):
         N_pro = inputs["data:propeller:number"]
         ESC_eta = inputs["data:esc:efficiency"]
         P_el_mot_hov = inputs["data:motor:power:hover"]
+        P_el_mot_takeoff = inputs["data:motor:power:takeoff"]
         t_hov = inputs["data:mission:hover:time"]
+        t_travel = inputs["data:mission:travel:time"]
         NRJ_Elevation = inputs["data:mission:travel:energy"]
         N_dis = inputs["data:mission:travel:displacement:number"]
 
         E_hover = (P_el_mot_hov * N_pro) / ESC_eta * t_hov
 
-        MTOW = inputs["data:system:MTOW:guess"]
-        H_dis = inputs["data:mission:travel:displacement:height"]
-        if self.options["use_potential_energy"]:
-            NRJ_Elevation_est = (MTOW * g * H_dis) * 2 / 0.8 / 0.8 / ESC_eta
+        # MTOW = inputs["data:system:MTOW:guess"]
+        # H_dis = inputs["data:mission:travel:displacement:height"]
+        if self.options["use_approximation"]:
+            # NRJ_Elevation_est = (MTOW * g * H_dis) * 2 / 0.8 / 0.8 / ESC_eta
+            NRJ_Elevation_est = (P_el_mot_takeoff * N_pro) / ESC_eta * t_travel
         else:
             NRJ_Elevation_est = NRJ_Elevation
         E_drone_est = (NRJ_Elevation_est + E_hover) * N_dis
@@ -68,7 +73,7 @@ class Mission(om.ExplicitComponent):
 
         N_dis_guess = np.floor(E_drone/E_drone_est*N_dis)
 
-        if self.options["use_potential_energy"]:
+        if self.options["use_approximation"]:
             E_drone = E_drone_est
 
         t_hov_max = E_drone/(P_el_mot_hov * N_pro) / ESC_eta
